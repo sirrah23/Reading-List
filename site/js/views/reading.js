14b755797e5e2cc6729daadb823df8a3e1bb3d5b
Sirrah23 <hseth93@gmail.com>
2016-09-15 00:52:12 -0400

    Can delete reading now

--- site/js/views/reading.js
+++ site/js/views/reading.js
@@ -4,6 +4,10 @@ app.ReadingView = Backbone.View.extend({
 
   tagName: 'div',
 
+  events: {
+      "click .delete" : "deleteReading" // class & id location is relative to tagName...
+  },
+
   className: 'readingContainer',
 
   template: _.template($('#readingTemplate').html()),
@@ -11,6 +15,13 @@ app.ReadingView = Backbone.View.extend({
   render: function(){
     this.$el.html(this.template(this.model.toJSON()));
     return this;
-  }
+  },
 
+  deleteReading: function(){
+      // Delete the model
+      this.model.destroy();  // As passed in to create the model, smh frameworks
+
+      // Delete the View
+      this.remove();
+  }
 });
