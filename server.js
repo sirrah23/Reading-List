3ead34131ec5f1a138935dd90abc0bb286286216
Sirrah23 <hseth93@gmail.com>
2016-09-20 23:31:49 -0400

    Can update and delete readings now

--- server.js
+++ server.js
@@ -42,6 +42,7 @@ app.get('/api', function(req, res){
   res.send('Library is running!');
 });
 
+//Get all readings
 app.get('/api/readings',function(req, res){
   return Reading.find(function(err, readings){
     if (! err){
@@ -52,13 +53,12 @@ app.get('/api/readings',function(req, res){
   });
 });
 
+//Create a new reading
 app.post('/api/readings', function(req, res){
-  console.log(req.body.title);
-  console.log(req.body.author);
-  console.log(req.body);
   let reading = new Reading({
     title: req.body.title,
-    author: req.body.author
+    author: req.body.author,
+    status: req.body.status
   });
   reading.save(function(err){
     if (!err){
@@ -68,6 +68,51 @@ app.post('/api/readings', function(req, res){
     }
   });
   return res.send(reading);
+});
+
+//Get a single reading by on id
+app.get('/api/readings/:id', function(req, res){
+  return Reading.findById(req.params.id, function(err, reading){
+    if(! err){
+      return res.send(reading);
+    } else {
+      return console.log(err);
+    }
+  });
+});
+
+//Update a reading after locating by id
+app.put('/api/readings/:id', function(req, res){
+  console.log("Updating book!");
+  return Reading.findById(req.params.id, function(err, reading){
+    reading.title = req.body.title;
+    reading.author = req.body.author;
+    reading.status = req.body.status;
+
+    reading.save(function(err){
+      if(! err){
+        console.log("Reading updated");
+      } else {
+        console.log(err);
+      }
+      return res.send(reading);
+    });
+  });
+});
+
+//Delete a reading after locating by id
+app.delete('/api/readings/:id', function(req, res){
+  console.log("Deleting book!");
+  return Reading.findById(req.params.id, function(err, reading){
+    reading.remove(function(err){
+      if(! err){
+        console.log("Reading removed");
+        return res.send('');
+      } else {
+        return console.log(err);
+      }
+    });
+  });
 });
 
 //START SERVER
